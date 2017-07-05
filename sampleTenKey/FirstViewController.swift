2f20b4dacfeecbe0151d88f3b3231e8b8ba882c2
ei17ringo <ei17ringo@gmail.com>
2017-07-05 21:36:06 +0800

    commit init

---
+++ sampleTenKey/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  sampleTenKey
+//
+//  Created by Eriko Ichinohe on 2017/07/05.
+//  Copyright © 2017年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var txtSample: UITextField!
+    
+    @IBAction func tapBtn(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sampleTenKey/tenKeyViewController.swift
@@ -0,0 +1,42 @@
+//
+//  tenKeyViewController.swift
+//  sampleTenKey
+//
+//  Created by Eriko Ichinohe on 2017/07/05.
+//  Copyright © 2017年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class tenKeyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapOne(_ sender: UIButton) {
+        
+        let fv = self.presentingViewController?.childViewControllers[0] as! FirstViewController
+        
+        fv.txtSample.text = "1"
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
